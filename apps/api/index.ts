bc987cf650b22e25970b649b5a7dee9270295fbf
Suryansh <suryanshvaish1@gmail.com>
2025-08-04 17:03:15 +0530

    added basic api

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.post("/website", async (req, res) => {
+  const { website } = req.body;
+  const startTime = Date.now();
+  const response = await fetch(website);
+  try {
+    const output = response.status === 200 ? "Good" : "Bad";
+    const endTime = Date.now();
+    const latency = endTime - startTime;
+    res.json({
+      mssg: output,
+      latency: latency,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.listen(8080, () => console.log("Running on port 8080"));
